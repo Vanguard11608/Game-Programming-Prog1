adb8b0cceb67e0cedcccb27e9655f5f48b39b9d7
Vanguard11608 <93083636+Vanguard11608@users.noreply.github.com>
2021-10-25 00:18:14 +1100

    Bhop main Proj file
    Incomplete:
    Issue with:
     Look();

---
+++ Bhop.cs
@@ -0,0 +1,174 @@
+
+
+using system;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform playerCam;
+    public Transform orientation;
+    private Rigidbody rb;
+
+    //Look
+    private float xRotation;
+    private float sensitivity = 50f;
+    private float sensMultiplier = 1f;
+
+   //Move
+    public float moveSpeed = 4500;
+    public float maxSpeed = 20;
+    public bool grounded;
+    public LayerMask whatIsGround;
+    public float counterMovement = 0.175f;
+    private float threshold = 0.01f;
+    public float maxSlopeAngle = 35f;
+
+    //Jump
+    private bool readyToJump = true;
+    private float jumpCooldown = 0.25f;
+    public float jumpForce = 550f;
+
+    //input
+    float x, y;
+    bool jumping;
+
+    private void MyInput() {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+        jumping = Input.GetButton("Jump");
+        }
+        private void Movement(){
+            rb.AddForce(Vector3.down * Time.deltaTime * 10); // More Grav
+            
+            Vector2 mag = FindVelRelativeToLook(); //find Realtive Vel. to where player is looking
+            float xMag = mag.x, yMag = mag.y;
+
+            CounterMovement(x, y, mag); 
+
+            if(readyToJump && jumping) Jump();
+
+            float maxSpeed = this.maxSpeed;
+        }
+            //max speed limiter
+        if (x > 0 && xMag > maxSpeed) x = 0;
+        if (x < 0 && xMag < -maxSpeed) x = 0;
+        if (y > 0 && yMag > maxSpeed) y = 0;
+        if (y < 0 && yMag < -maxSpeed) y = 0;
+
+        float multiplier = 1f, multiplierV = 0f;
+
+        if (!grounded) {
+            multiplier = 0.5f;
+            multiplierV = 0.5f;
+        }
+
+        rb.AddForce(orientation.transform.forward * y * moveSpeed * Time.deltaTime * multiplier * multiplierV);
+        rb.AddForce(orientation.transform.right * x * moveSpeed * Time.deltaTime * multiplier);
+        
+}
+
+    private void Jump() {
+        if (grounded && readyToJump){
+            readyToJump = false:
+
+            rb.AddForce(Vector2.up * jumpForce * 1.5f);
+            rb.AddForce(normalVector * jumpForce * 0.5f);
+
+            Vector3 vel = rb.velocity;
+            if (rb.velocity.y < 0.5f)
+            rb.velocity = new Vector3(vel.x, 0, vel.z);
+            else if (rb.velocity.y > 0)
+            rb.velocity = new Vector3(vel.x, vel.y / 2, vel.z);
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+
+        }
+    }
+
+    private void ResetJump() {
+        readyToJump = true;
+    }
+
+    private float desiredX;
+    private void look (){
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
+        float mosuseY = Input.GetAxis("Mouse Y") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
+
+        Vector3 rot = playerCam.transform.localRotation.eulerAngles;
+        desiredX = rot.y + mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerCam.transform.localRotation = Quaternion.Euler(xRotation, desiredX, 0);
+        orientation.transform.localRotation = Quaternion.Euler(0, desiredX, 0);
+            }
+
+            private void CounterMovement(float x, float y, Vector2 mag){
+                if (!grounded || jumping) return;
+            }
+
+            if (Math.Abs(mag.x) > threshold && Math.Abs(x) < 0.05f || (mag.x < -threshold && x > 0) || (mag.x >threshold && x < 0)){
+                rb.AddForce(moveSpeed * orientation.transform.right * Time.deltaTime * -mag.x * counterMovement);
+            }
+            if(Math.Abs(mag.y) > threshold && math.Abs(y) < 0.05f || (mag.y < -threshold && y > 0)|| (mag.y > threshold && y <00)){
+                rb.AddForce(moveSpeed * orientation.transform.forward * Time.deltaTime * -mag.y * counterMovement);
+              }
+
+              if (Mathf.Sprt((Mathf.Pow(rb.velocity.x, 2) + Mathf.Pow(rb.velocity.z, 2))) > maxSpeed){
+                  float fallspeed = rb.velocity.y;
+                  Vector3 n = rb.velocity.normalized * maxSpeed;
+                  rb.velocity = new Vector3(n.x, fallspeed, n.z);
+              }
+            }
+
+            public Vector2 FindVelRelativeToLook() {
+                float lookAngle = orientation.transform.eulerAngles.y;
+                float moveAngle = Mathf.Atan2(rb.velocity.x, rb.velocity.z) * Mathf.Rad2Deg;
+
+                float u = Mathf.DeltaAngle(lookAngle, moveAngle);
+                float v = 90 - u;
+
+                float magnitue = rb.velocity.magnitude
+                float yMag = magnitue * Mathf.Cos(u * Mathf.Deg2Rad);
+                float xMag = magnitue * Mathf.Cos(v * Mathf.Deg2Rad);
+
+                return new Vector2(xMag, yMag);
+            }
+
+            private bool IsFloor(Vector3 v){
+                float angle = Vector3.Angle(Vector3.up, v);
+                return angle < maxSlopeAngle;
+            }
+
+            private bool cancellingGrounded;
+
+            private void OnCollisionStay(Collision other){
+                int layer = other.gameObject.layer;
+                if (whatIsGround != (whatIsGround | (1 << layer))) return;
+
+                for (int i = 0; i < other.contactCount; i++){
+                    Vector3 normal = other.contacts[i].normal;
+
+                    if (IsFloor(normal)) {
+                        grounded = true;
+                        cancellingGrounded = false;
+                        normalVector = normal;
+                        CancelInvoke(nameof(StopGrounded));
+
+                    }
+                }
+
+                float delay = 3f;
+                if (!cancellingGrounded){
+                    cancellingGrounded = true;
+                    Invoke(nameof(StopGrounded),Time.deltaTime * delay);
+                }
+            }
+
+            private void StopGrounded(){
+                grouned = false;
+
+                }
+
+            }
