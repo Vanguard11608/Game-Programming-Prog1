3f3a270d41f85ab181425523b37fdd9ef5572d13
Vanguard11608 <93083636+Vanguard11608@users.noreply.github.com>
2021-11-06 10:43:33 +1100

    Add files via upload
    Made it into a CS file

---
+++ DeathBox.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathBox : MonoBehaviour{
+
+void OnCollisionEnter(Collision coll){
+
+//if colliding with game object with the tag player
+if (coll.gameObject.tag == "Player"){ 
+
+//load current active scene
+SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+	}
+   }
+}
